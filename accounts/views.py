57d62585f67c40efa6015e9ae1449d23c5eb090f
Nicolás Butterfield <nicobutter@gmail.com>
2025-01-31 18:16:32 -0300

    sttings updates for statics files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, FormView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
-# Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'  # Plantilla para el login
+    form_class = AuthenticationForm  # Formulario de autenticación de Django
+    redirect_authenticated_user = True  # Redirige a los usuarios ya autenticados
+
+    def get_success_url(self):
+        # Redirige según el rol del usuario
+        if self.request.user.rol == 'admin':
+            return reverse_lazy('admin_dashboard')  # URL para el dashboard del admin
+        else:
+            return reverse_lazy('user_dashboard')  # URL para el dashboard del usuario
--- el_comandero/urls.py
+++ el_comandero/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+     path('', RedirectView.as_view(url=reverse_lazy('login')), name='home'),
 ]
